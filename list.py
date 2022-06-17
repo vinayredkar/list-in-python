00586248c45888526dc4e3fb21a95bcc638c5adf
vinayredkar <86841694+vinayredkar@users.noreply.github.com>
2022-06-17 20:39:55 +0530

    #creating a list output of giving positive no.

---
+++ list.py
@@ -0,0 +1,10 @@
+#creating a list output of giving positive no.
+list1 = [12, -7, 5, 64, -14]
+for num in list1:
+    if(num>=0):
+        print(num, end=" ")
+
+list2 =[12,14,-95,3]
+for a in list2:
+    if(a>=0):
+        print(a,end=" ")
